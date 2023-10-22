15d74837d5e7b4dc743a0a6d6316d7c292097bf3
Powermoneys <powewewe@mail.ru>
2023-10-22 19:38:38 +0300

    3

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,24 +1,12 @@
-// const { Sequelize } = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-// module.exports = new Sequelize(
-//   process.env.REACT_APP_DATABASE,
-//   process.env.REACT_APP_USERNAME,
-//   process.env.REACT_APP_PASSWORD,
-//   {
-//     dialect: "postgres",
-//     host: process.env.REACT_APP_HOST,
-//     port: process.env.REACT_APP_DBPORT,
-//   }
-// );
-
-
-const { Pool } = require("pg");
-
-const connectionString =
-  "postgresql://postgres:fzXT2yZpdhjJ56C2KyNf@containers-us-west-180.railway.app:7224/railway";
-
-const pool = new Pool({
-  connectionString,
-});
-
-module.exports = pool;
+module.exports = new Sequelize(
+  process.env.REACT_APP_DATABASE,
+  process.env.REACT_APP_USERNAME,
+  process.env.REACT_APP_PASSWORD,
+  {
+    dialect: "postgres",
+    host: process.env.REACT_APP_HOST,
+    port: process.env.REACT_APP_DBPORT,
+  }
+);
