51eb78d9be86567ebfdfbe57d7d65bfbca71c59b
Powermoneys <powewewe@mail.ru>
2023-10-24 00:09:46 +0300

    фиксы аутентификации и авторизации

--- backend/src/routes/itemRouter.js
+++ backend/src/routes/itemRouter.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const itemController = require('../controllers/itemController');
+const { authenticate, authorize } = require('../middlewares/auth');
 
 // Создание роутера
 const itemRouter = express.Router();
 
 // Настройка маршрутов с использованием контроллеров
-itemRouter.post('/', itemController.createItem);
-itemRouter.put('/:id', itemController.updateItem);
-itemRouter.delete('/:id', itemController.deleteItem);
+itemRouter.post('/', authenticate, authorize, itemController.createItem);
+itemRouter.put('/:id', authenticate, authorize, itemController.updateItem);
+itemRouter.delete('/:id', authenticate, authorize, itemController.deleteItem);
 itemRouter.get('/', itemController.listItems);
 
 module.exports = itemRouter;
